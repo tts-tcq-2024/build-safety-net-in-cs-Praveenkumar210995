620ed5c7ad226953c7d04c272325a3e4b4aaa7b2
Praveenkumar210995 <pravin21995@gmail.com>
2024-09-03 14:25:41 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,68 +1,72 @@
 using System;
+using System.Collections.Generic;
 using System.Text;
 
-public class Soundex
+namespace SoundexCheck
 {
-    public static string GenerateSoundex(string name)
+    public class Soundex
     {
-        if (string.IsNullOrEmpty(name))
-        {
-            return string.Empty;
-        }
 
-        StringBuilder soundex = new StringBuilder();
-        soundex.Append(char.ToUpper(name[0]));
-        char prevCode = GetSoundexCode(name[0]);
-
-        for (int i = 1; i < name.Length && soundex.Length < 4; i++)
+        private static void AppendCode(string name, StringBuilder soundex, char prevCode)
         {
-            char code = GetSoundexCode(name[i]);
-            if (code != '0' && code != prevCode)
+            for (int i = 1; i < name.Length && soundex.Length < 4; i++)
             {
-                soundex.Append(code);
-                prevCode = code;
+                char code = GetCode(name[i]);
+                if (IsValidCode(code, prevCode))
+                {
+                    soundex.Append(code);
+                    prevCode = code;
+                }
             }
         }
 
-        while (soundex.Length < 4)
+        private static char GetCode(char c)
         {
-            soundex.Append('0');
+            char code;
+            c = char.ToUpper(c);
+            var soundexMap = new Dictionary<char, char>
+            {
+                {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+                {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'},
+                {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
+                {'D', '3'}, {'T', '3'},
+                {'L', '4'},
+                {'M', '5'}, {'N', '5'},
+                {'R', '6'}
+            };
+
+            return soundexMap.TryGetValue(c, out code) ? code : '0';
         }
 
-        return soundex.ToString();
-    }
-
-    private static char GetSoundexCode(char c)
-    {
-        c = char.ToUpper(c);
-        switch (c)
+        private static bool IsValidCode(char code, char prevCode)
         {
-            case 'B':
-            case 'F':
-            case 'P':
-            case 'V':
-                return '1';
-            case 'C':
-            case 'G':
-            case 'J':
-            case 'K':
-            case 'Q':
-            case 'S':
-            case 'X':
-            case 'Z':
-                return '2';
-            case 'D':
-            case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M':
-            case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y
+            return code != 0 && code != prevCode;
+        }
+
+        private static void AddZero(StringBuilder soundex)
+        {
+            while (soundex.Length < 4)
+            {
+                soundex.Append(0);
+            }
+        }
+
+        public static string GenerateSoundex(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return string.Empty;
+            }
+
+            StringBuilder soundex = new StringBuilder();
+            soundex.Append(char.ToUpper(name[0]));
+            char prevCode = GetCode(name[0]);
+
+            AppendCode(name, soundex, prevCode);
+
+            AddZero(soundex);
+
+            return soundex.ToString();
         }
     }
 }
